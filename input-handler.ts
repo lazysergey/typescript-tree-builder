780c866f5ff6a69cf95370ae66526ee71de57b4f
awwward <->
2019-04-26 21:29:31 +0300

    depth comparisons now on Persons not names

--- input-handler.ts
+++ input-handler.ts
@@ -1,44 +1,44 @@
-import { fromEvent } from 'rxjs/observable/fromEvent';
-import { debounceTime, map, distinctUntilChanged, filter, tap } from 'rxjs/operators';
-import { Observable } from 'rxjs';
-import { combineLatest } from 'rxjs/observable/combineLatest';
+    import { fromEvent } from 'rxjs/observable/fromEvent';
+    import { debounceTime, map, distinctUntilChanged, filter, tap } from 'rxjs/operators';
+    import { Observable } from 'rxjs';
+    import { combineLatest } from 'rxjs/observable/combineLatest';
 
-export class InputHandler {
-  private _name1Input: HTMLInputElement;
-  private _name2Input: HTMLInputElement;
-  private _resultContainer: HTMLElement;
+    export class InputHandler {
+    private _name1Input: HTMLInputElement;
+    private _name2Input: HTMLInputElement;
+    private _resultContainer: HTMLElement;
 
-  constructor() {
-    this._name1Input = document.getElementById("name1") as HTMLInputElement;
-    this._name2Input = document.getElementById("name2") as HTMLInputElement;
-    this._resultContainer = document.getElementById("result");
-  }
+    constructor() {
+        this._name1Input = document.getElementById("name1") as HTMLInputElement;
+        this._name2Input = document.getElementById("name2") as HTMLInputElement;
+        this._resultContainer = document.getElementById("result");
+    }
 
-  private _createEventObservable(target: HTMLInputElement) {
-    return fromEvent(target, "keyup").pipe(
-      debounceTime(300),
-      map((event: KeyboardEvent) => this._getValue(event)),
-      filter(value => !!value),
-      distinctUntilChanged()
-    )
-  }
+    private _createEventObservable(target: HTMLInputElement) {
+        return fromEvent(target, "keyup").pipe(
+        debounceTime(300),
+        map((event: KeyboardEvent) => this._getValue(event)),
+        filter(value => !!value),
+        distinctUntilChanged()
+        )
+    }
 
-  private _getValue(event: KeyboardEvent){
-    return (event.target as HTMLInputElement).value;
-  }
+    private _getValue(event: KeyboardEvent){
+        return (event.target as HTMLInputElement).value;
+    }
 
-  onKeyUp(): Observable<any> {
-    return combineLatest(
-      this._createEventObservable(this._name1Input),
-      this._createEventObservable(this._name2Input),
-      (value1, value2) => ({
-        input1: value1,
-        input2: value2
-      })
-    )
-  }
+    onKeyUp(): Observable<any> {
+        return combineLatest(
+        this._createEventObservable(this._name1Input),
+        this._createEventObservable(this._name2Input),
+        (value1, value2) => ({
+            input1: value1,
+            input2: value2
+        })
+        )
+    }
 
-  set result(value: string) {
-    this._resultContainer.innerText = value;
-  }
-}
+    set result(value: string) {
+        this._resultContainer.innerHTML = value;
+    }
+    }
--- tree-builder.service.ts
+++ tree-builder.service.ts
@@ -1,119 +1,117 @@
-import { Person } from "./person";
+  import { Person } from "./person";
 
-export class TreeBuilderService {
-  private _tree: Person[] = [];
-  private _input: { [name: string]: string[] };
-  constructor() { }
+  export class TreeBuilderService {
+    private _tree: Person[] = [];
+    private _input: { [name: string]: string[] };
+    constructor() { }
 
-  buildTree(input): Person[] {
-    this._input = input;
-    this._parseRoots().forEach(
-      root => {
-        this._createNodes(root);
-      }
-    );
-    this._tree.forEach(node => {
-      this._buildHtml(node, null);
-    });
-    return this._tree;
-  }
-
-  private _buildHtml(node: Person, parentElement: HTMLElement) {
-    parentElement = parentElement ? parentElement.querySelector('li') : document.getElementById('tree');
-    const newUlElement = document.createElement("ul");
-    newUlElement.innerHTML += `<li title="depth: ${node.depth}">${node.name}</li>`;
-    parentElement.append(newUlElement);
-    node.children.forEach(
-      child => this._buildHtml(child, newUlElement || parentElement)
-    )
-  }
-
-  private _createNodes(name: string, parent: Person = null) {
-    let arrayForCurrentNode = parent ? parent.children : this._tree;
-    const newPerson = new Person(name, parent ? parent : null, [], (parent ? (parent.depth + 1) : 0));
-    arrayForCurrentNode.push(newPerson);
-    if (this._input[name]) {
-      this._input[name].forEach(child => {
-        this._createNodes(child, newPerson);
+    buildTree(input): Person[] {
+      this._input = input;
+      this._parseRoots().forEach(
+        root => {
+          this._createNodes(root);
+        }
+      );
+      this._tree.forEach(node => {
+        this._buildHtml(node, null);
       });
+      return this._tree;
     }
-  }
 
-  private _parseRoots() {
-    let allChildrenIds = [];
-    let allParentsIds = [];
-    Object.keys(this._input).forEach(person => {
-      allParentsIds.push(person);
-      let currentChildren = this._input[person];
-      currentChildren.forEach(childId => {
-        allChildrenIds.push(childId);
-      })
-    });
+    private _buildHtml(node: Person, parentElement: HTMLElement) {
+      parentElement = parentElement ? parentElement.querySelector('li') : document.getElementById('tree');
+      const newUlElement = document.createElement("ul");
+      newUlElement.innerHTML += `<li title="depth: ${node.depth}">${node.name}</li>`;
+      parentElement.append(newUlElement);
+      node.children.forEach(
+        child => this._buildHtml(child, newUlElement || parentElement)
+      )
+    }
 
-    allChildrenIds.forEach(c => {
-      const parentIndex = allParentsIds.findIndex(p => p == c);
-      if (parentIndex >= 0) {
-        allParentsIds.splice(parentIndex, 1)
+    private _createNodes(name: string, parent: Person = null) {
+      let arrayForCurrentNode = parent ? parent.children : this._tree;
+      const newPerson = new Person(name, parent ? parent : null, [], (parent ? (parent.depth + 1) : 0));
+      arrayForCurrentNode.push(newPerson);
+      if (this._input[name]) {
+        this._input[name].forEach(child => {
+          this._createNodes(child, newPerson);
+        });
       }
-    })
-    return allParentsIds;
-  }
-
-  public findRelations(name1: string, name2: string) {
-    const depth = this._getDepthDifference(name1, name2);
-    let relationKey;
-    if(!depth){
-      return "not relatives";
     }
-    if(depth < 0) {
-      relationKey = "Father";
-    } else {
-      relationKey = "Child";
-    }      
-    return `${name1} is ${"Grand".repeat(Math.abs(depth) - 1)}${relationKey} of ${name2}`
-  }
 
-  private _getDepthDifference(name1: string, name2: string){
-    let person1: Person, person2: Person;
-    if(this._isInSameRoot(name1, name2)){
-      person1 = this.findPerson(name1);
-      // console.log(person1);
-      person2 = this.findPerson(name2);
-      //  console.log(person2);
+    private _parseRoots() {
+      let allChildrenIds = [];
+      let allParentsIds = [];
+      Object.keys(this._input).forEach(person => {
+        allParentsIds.push(person);
+        let currentChildren = this._input[person];
+        currentChildren.forEach(childId => {
+          allChildrenIds.push(childId);
+        })
+      });
+
+      allChildrenIds.forEach(c => {
+        const parentIndex = allParentsIds.findIndex(p => p == c);
+        if (parentIndex >= 0) {
+          allParentsIds.splice(parentIndex, 1)
+        }
+      })
+      return allParentsIds;
+    }
+
+    public findRelations(name1: string, name2: string) {
+      let person1: Person = this.findPerson(name1);
+      if (!person1) {
+        return `${name1} not found`;
+      }
+      let person2: Person = this.findPerson(name2);
+      if (!person2) {
+        return `${name2} not found`;
+      }
+      
+      let depth = this._getDepthDelta(person1, person2);    
+      let relationKey;
+      if (!depth) {
+        return "Not relatives";
+      } else if (depth < 0) {
+        if (person1.descendants.includes(person2)){
+          relationKey = "Father";
+        } else {
+          relationKey = "Uncle"
+        }
+      } else {
+        if (person2.descendants.includes(person1)){
+          relationKey = "Child";
+        } else {
+          relationKey = "Nephew"
+        }
+      }
+      return `${person1.name} is <span>${"Grand".repeat(Math.abs(depth) - 1)}${relationKey}</span> of ${person2.name}`
+    }
+
+    private _getDepthDelta(person1: Person, person2: Person): number {
       return person1.depth - person2.depth;
-    } else {
+    }
+
+    public findPerson(nameToSearch: string): Person {
+      let root = this.findRoot(nameToSearch);
+      return root ? this.searchRecursively(root, nameToSearch) : null;
+    }
+
+    public findRoot(nameToSearch) {
+      return this._tree.find(root => root.descendants.map(d => d.name.toLowerCase()).includes(nameToSearch.toLowerCase()) || root.name.toLowerCase() == nameToSearch.toLowerCase());
+    }
+
+    private searchRecursively(person, nameToSearch): Person {
+      if (person.name.toLowerCase() == nameToSearch.toLowerCase()) {
+        return person;
+      } else if (person.children) {
+        let result = null;
+        for (let i = 0; result == null && i < person.children.length; i++) {
+          result = this.searchRecursively(person.children[i], nameToSearch);
+        }
+        return result;
+      }
       return null;
     }
   }
-
-  private _isInSameRoot(name1, name2){
-    const root1 = this.findRoot(name1);
-    // console.log(root1)
-    const root2 = this.findRoot(name2);
-    // console.log(root2);
-    // console.log("sameroot: ",root1 === root2);
-    return root1 === root2;
-  }
-
-  public findPerson(nameToSearch: string) {
-    let root = this.findRoot(nameToSearch);
-    return root? this.searchRecursively(root, nameToSearch) : null;
-  }
-
-  public findRoot(nameToSearch){
-    return this._tree.find(root => root.descendants.map(d => d.name).includes(nameToSearch) || root.name == nameToSearch);
-  }
-
-  private searchRecursively(person, nameToSearch) {
-    if (person.name == nameToSearch) {
-      return person;
-    } else if (person.children) {
-      let result = null;
-      for (let i = 0; result == null && i < person.children.length; i++) {
-        result = this.searchRecursively(person.children[i], nameToSearch);
-      }
-      return result;
-    }
-    return null;
-  }
-}
